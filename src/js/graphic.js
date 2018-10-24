9baf76066e91d0cd8a629445ede8903ba793933a
Jan Diehm <jandiehm@Jans-MBP-2.lan1>
2018-10-24 10:43:48 -0500

    setting up plateau chart

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,9 +1,28 @@
 /* global d3 */
+import './pudding-chart/plateau-template'
+
+// data
+let data = null
 
 function resize() {}
 
+function loadData(){
+  return new Promise((resolve, reject) => {
+    d3.loadData('assets/data/allRuns_wAdds.csv', (err, response) => {
+      if (err) reject(err)
+      else resolve(response)
+      data = response
+    })
+  })
+}
+
 function init() {
-	console.log('Make something awesome!');
+  return new Promise((resolve) => {
+    loadData()
+      .then(response => {
+        //console.log(data)
+      })
+  })
 }
 
 export default { init, resize };
